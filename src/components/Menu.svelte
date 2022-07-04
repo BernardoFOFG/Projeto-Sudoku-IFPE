973c5dde3ea7ba2745c9e845047cf0ac70e16aa3
BernardoFOFG <bernardofofg@gmail.com>
2022-07-04 05:42:27 -0300

    modify Menu.svelte

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,39 +1,30 @@
-<svelte:head> 
-  <link rel="stylesheet" href="../../styles/menu.css">
-</svelte:head>
-
 <!-- Script -->
 <script>
   import { status } from "../../scripts/Status.js";
   import { statusPage } from "../../scripts/Status.js";
 </script>
 
+<svelte:head>
+  <link rel="stylesheet" href="../../styles/menu.css" />
+</svelte:head>
+
 <!-- HTML -->
 
 <main>
-  <div class="container">
-    <section class="menu">
-      <div class="box">
-        <div id="logos">
-          <h1>Projeto SUDOKU</h1>
-          <img
-            src="https://i.ibb.co/s5BDp2R/logo-sudoku.png"
-            alt="Sudoku Logo"
-          />
-          <img
-            src="https://ingresso.ifpe.edu.br/static/img/ifpe-logo.svg"
-            alt="IFPE Logo"
-          />
-        </div>
-        <div class="buttons">
-          <div class="status" on:click={() => statusPage("jogar")}>
-            Jogar</div>
-          <div class="status" on:click={() => statusPage("sobre")}>
-            Sobre</div>
-          <div class="status" on:click={() => statusPage("ajuda")}>
-            Ajuda</div>
-        </div>
+  <section class="menu">
+    <div class="box">
+      <h1>Projeto Sudoku</h1>
+      <div id="logos">
+        <img
+          src="https://ingresso.ifpe.edu.br/static/img/ifpe-logo.svg"
+          alt="IFPE Logo"
+        />
       </div>
-    </section>
-  </div>
+      <div class="buttons">
+        <div class="status" on:click={() => statusPage("jogar")}>Jogar</div>
+        <div class="status" on:click={() => statusPage("sobre")}>Sobre</div>
+        <div class="status" on:click={() => statusPage("ajuda")}>Ajuda</div>
+      </div>
+    </div>
+  </section>
 </main>
