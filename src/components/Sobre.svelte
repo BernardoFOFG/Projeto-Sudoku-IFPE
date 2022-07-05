0b5bd7189b964775b98b0d214c7b8a503dd1148c
BernardoFOFG <bernardofofg@gmail.com>
2022-07-05 00:14:14 -0300

    feat: add new components

--- src/components/Sobre.svelte
+++ src/components/Sobre.svelte
@@ -3,17 +3,34 @@
   import Start from "./Start.svelte";
   let curso = "Tecnologia em Sistema para Internet";
   class Infos {
-    constructor(nome, idade, curso) {
+    constructor(nome, idade, curso, descricao, linkedin, instagram, github) {
       this.nome = nome;
       this.idade = idade;
       this.curso = curso;
+      this.descricao = descricao;
+      this.linkedin = linkedin;
+      this.instagram = instagram;
+      this.github = github;
     }
   }
-  let info1 = new Infos("Bernardo Filipe", "19", curso);
-  let info2 = new Infos("Carolayne Russel", "19", curso);
-  let info3 = new Infos("Eduardo Silvino", "18", curso);
-  let info4 = new Infos("Kallyanne Vitória", "19", curso);
-  let info5 = new Infos("Valdir Kehrle", "36", curso);
+
+  let infos = [
+    new Infos(
+      "Bernardo Filipe",
+      "19",
+      curso,
+      "Olá, sou estudante do 1º período do curso de tecnologia em sistema\
+          para internet, e sou entusiasta em desenvolvimento front-end, mas meu\
+          objetivo é meio academico, desenvolver pesquisas, e futuramente\
+          iniciar uma carreira academico na docência.",
+      "https://www.linkedin.com/in/bernardofofg/",
+      "https://www.instagram.com/bernardofofg/"
+    ),
+    new Infos("Carolayne Russel", "19", curso, "Olá", "#", "#", "#"),
+    new Infos("Eduardo Silvino", "18", curso, "Olá", "#", "#", "#"),
+    new Infos("Kallyanne Vitória", "19", curso, "Olá", "#", "#", "#"),
+    new Infos("Valdir Kehrle", "36", curso, "Olá", "#", "#", "#"),
+  ];
 </script>
 
 <!-- HTML -->
@@ -28,100 +45,29 @@
     <!-- svelte-ignore a11y-missing-attribute -->
     <img src="https://i.ibb.co/F6PjnG6/undraw-Engineering-team-re-fvat.png" />
     <div class="team__participants">
-      <div class="team__participants__box">
-        <div class="team__participants__box__icons">
-          <a href="https://www.linkedin.com/in/bernardofofg/" target="_blank">
-            <img
-              src="https://icongr.am/simple/linkedin.svg?size=32&color=currentColor&colored=false"
-              alt="LinkedIn-Icon"
-            />
-          </a>
-          <a href="##">
-            <img
-              src="https://icongr.am/simple/instagram.svg?size=32&color=currentColor&colored=false"
-              alt="Instagram-Icon"
-            />
-          </a>
+      {#each infos as info}
+        <div class="team__participants__box">
+          <div class="team__participants__box__icons">
+            <a href={info.linkedin} target="_blank">
+              <img
+                src="https://icongr.am/simple/linkedin.svg?size=32&color=currentColor&colored=false"
+                alt="LinkedIn-Icon"
+              />
+            </a>
+            <a href="##">
+              <img
+                src="https://icongr.am/simple/instagram.svg?size=32&color=currentColor&colored=false"
+                alt="Instagram-Icon"
+              />
+            </a>
+          </div>
+          <p>{info.nome}</p>
+          <p class="resume">
+            {info.descricao}
+          </p>
         </div>
-        <p>{info1.nome}</p>
-        <p class="resume">
-          Olá, sou estudante do 1º período do curso de tecnologia em sistema
-          para internet, e sou entusiasta em desenvolvimento front-end, mas meu
-          objetivo é meio academico, desenvolver pesquisas, e futuramente
-          iniciar uma carreira academico na docência
-        </p>
-      </div>
-      <div class="team__participants__box">
-        <div class="team__participants__box__icons">
-          <a href="##" target="_blank">
-            <img
-              src="https://icongr.am/simple/linkedin.svg?size=32&color=currentColor&colored=false"
-              alt="LinkedIn-Icon"
-            />
-          </a>
-          <a href="##"
-            ><img
-              src="https://icongr.am/simple/instagram.svg?size=32&color=currentColor&colored=false"
-              alt="Instagram-Icon"
-            />
-          </a>
-        </div>
-        <p>{info2.nome}</p>
-      </div>
-      <div class="team__participants__box">
-        <div class="team__participants__box__icons">
-          <a href="##">
-            <img
-              src="https://icongr.am/simple/linkedin.svg?size=32&color=currentColor&colored=false"
-              alt="LinkedIn-Icon"
-            />
-          </a>
-          <a href="##"
-            ><img
-              src="https://icongr.am/simple/instagram.svg?size=32&color=currentColor&colored=false"
-              alt="Instagram-Icon"
-            />
-          </a>
-        </div>
-        <p>{info3.nome}</p>
-      </div>
-      <div class="team__participants__box">
-        <div class="team__participants__box__icons">
-          <a href="##"
-            ><img
-              src="https://icongr.am/simple/linkedin.svg?size=32&color=currentColor&colored=false"
-              alt="LinkedIn-Icon"
-            />
-          </a>
-          <a href="##"
-            ><img
-              src="https://icongr.am/simple/instagram.svg?size=32&color=currentColor&colored=false"
-              alt="Instagram-Icon"
-            />
-          </a>
-        </div>
-        <p>{info4.nome}</p>
-      </div>
-      <div class="team__participants__box">
-        <div class="team__participants__box__icons">
-          <a href="##"
-            ><img
-              src="https://icongr.am/simple/linkedin.svg?size=32&color=currentColor&colored=false"
-              alt=""
-            />
-          </a>
-          <a href="##"
-            ><img
-              src="https://icongr.am/simple/instagram.svg?size=32&color=currentColor&colored=false"
-              alt="Instagram-Icon"
-            />
-          </a>
-        </div>
-        <p>{info5.nome}</p>
-      </div>
+      {/each}
     </div>
   </section>
   <Start />
 </main>
-
-<!-- CSS -->
